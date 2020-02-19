2740fe6b09591955a0dcf9423b59045e33c66b98
Sabin Lungu <sabinlungu293@gmail.com>
2020-02-19 11:34:09 +0000

    Added order summary to Payment Activity

--- app/src/main/java/com/example/weshopapplication/SendInvoiceAPI.java
+++ app/src/main/java/com/example/weshopapplication/SendInvoiceAPI.java
@@ -1,4 +1,7 @@
 package com.example.weshopapplication;
 
+// Author of Application/Class: Sabin Constantin Lungu
+// Purpose of Application/Class: This class uses the JavaMail API that automatically sends an e-mail to the user after they have purchased a specific product(s)
+
 public class SendInvoiceAPI {
 }
