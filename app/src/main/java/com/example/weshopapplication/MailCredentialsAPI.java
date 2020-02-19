f19c69df9dac7e1a7f3fcb27c98c367e0b0a7341
Sabin Lungu <sabinlungu293@gmail.com>
2020-02-19 09:51:51 +0000

    Added SQLIte DB Code.

---
+++ app/src/main/java/com/example/weshopapplication/MailCredentialsAPI.java
@@ -0,0 +1,6 @@
+package com.example.weshopapplication;
+
+public class MailCredentialsAPI {
+    public final String EMAIL_ADDRESS = "sabinlungu293@gmail.com"; // The test e-mail address to send to
+    public final String PASSWORD = ""; // The password
+}
