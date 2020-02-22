1d3386b73f9e3b32d3ed9cfe65c9012f9371b62b
Sabin Lungu <sabinlungu293@gmail.com>
2020-02-22 13:25:06 +0000

    Finished UI Implementation for Clothing Category

--- app/src/main/java/com/example/weshopapplication/ClothingCategory.java
+++ app/src/main/java/com/example/weshopapplication/ClothingCategory.java
@@ -1,14 +1,44 @@
 package com.example.weshopapplication;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ClothingCategory extends AppCompatActivity {
+public class ClothingCategory extends AppCompatActivity implements View.OnClickListener {
+    private Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clothing_category);
+
+        this.nextButton = findViewById(R.id.nextPageBtn);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        try {
+
+            if (v.getId() == R.id.nextPageBtn) {
+                Intent intent = new Intent(ClothingCategory.this, ClothingActivityTwo.class);
+                startActivity(intent);
+
+            }
+
+        } catch (ActivityNotFoundException exc) {
+
+            Log.d("Error", exc.toString());
+        }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
