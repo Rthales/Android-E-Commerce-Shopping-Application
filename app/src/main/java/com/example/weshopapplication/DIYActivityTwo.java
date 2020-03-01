16756d93f872422010aeb14c4fed5c222733708a
Sabin Constantin Lungu (2B || !2B) <sabinlungu293@gmail.com>
2020-03-01 18:51:41 +0000

    Implementing DIY Activity 2 interface.

---
+++ app/src/main/java/com/example/weshopapplication/DIYActivityTwo.java
@@ -0,0 +1,14 @@
+package com.example.weshopapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DIYActivityTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_d_i_y_two);
+    }
+}
