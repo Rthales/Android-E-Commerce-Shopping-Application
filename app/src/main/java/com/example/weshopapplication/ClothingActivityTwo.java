ebff529b8f89ea01e766fe32b85cc9313a0dec49
Sabin Constantin Lungu (2B || !2B) <sabinlungu293@gmail.com>
2020-03-01 22:17:47 +0000

    Finished Implementing DIY Activity 2 UI

--- app/src/main/java/com/example/weshopapplication/ClothingActivityTwo.java
+++ app/src/main/java/com/example/weshopapplication/ClothingActivityTwo.java
@@ -1,14 +1,89 @@
 package com.example.weshopapplication;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.HashMap;
+
 public class ClothingActivityTwo extends AppCompatActivity {
+    private ImageView cartIcon;
+    private HashMap<Integer, Products> listOfProductsToAddToBasket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clothing_two);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) { // Add the toolbar menu
+        // Inflate the activities menu
+        MenuInflater activityInflater = getMenuInflater(); // Get the activity inflater
+        activityInflater.inflate(R.menu.homepagemenu, menu);
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.basket_action_button, menu);
+
+        View view = menu.findItem(R.id.cart_menu).getActionView();
+
+        cartIcon = view.findViewById(R.id.cart_icon);
+
+        cartIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent basketIntent = new Intent(ClothingActivityTwo.this, BasketActivity.class); // Create a basket intent
+                basketIntent.putExtra("map", listOfProductsToAddToBasket); // Transit over the hash map data to the basket
+                startActivity(basketIntent); // Start the intent
+            }
+        });
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        try {
+            switch (item.getItemId()) {
+                case R.id.sportsAndOutdoorsCategory:
+                    Intent sportsCategory = new Intent(ClothingActivityTwo.this, SportsAndOutdoorsActivity.class);
+                    startActivity(sportsCategory);
+
+                    break;
+
+                case R.id.techCategory:
+                    Intent techActivity = new Intent(ClothingActivityTwo.this, TechActivity.class);
+                    startActivity(techActivity);
+                    break;
+
+                case R.id.clothingCategory:
+                    Intent clothingActivity = new Intent(ClothingActivityTwo.this, ClothingCategory.class);
+                    startActivity(clothingActivity);
+                    break;
+
+                case R.id.diyCategory:
+                    Intent diyActivity = new Intent(ClothingActivityTwo.this, DIYActivity.class);
+                    startActivity(diyActivity);
+                    break;
+
+                default:
+                    super.onOptionsItemSelected(item);
+
+            }
+
+        } catch (ActivityNotFoundException exc) {
+            Log.d(String.valueOf(R.string.error), exc.toString());
+        }
+
+        return true;
     }
 }
--- app/src/main/java/com/example/weshopapplication/ClothingCategory.java
+++ app/src/main/java/com/example/weshopapplication/ClothingCategory.java
@@ -1,7 +1,9 @@
 package com.example.weshopapplication;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -20,10 +22,13 @@ public class ClothingCategory extends AppCompatActivity {
             @Override
 
             public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(ClothingCategory.this, ClothingActivityTwo.class);
+                    startActivity(intent);
 
-                Intent intent = new Intent(ClothingCategory.this, ClothingActivityTwo.class);
-                startActivity(intent);
-
+                } catch (ActivityNotFoundException exc) {
+                    Log.d(String.valueOf(R.string.error), exc.toString());
+                }
             }
         });
 
