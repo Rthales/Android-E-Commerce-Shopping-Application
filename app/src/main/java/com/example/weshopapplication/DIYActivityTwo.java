c20840f56bfb8d1dc13a41a170e5a962d0267e35
Sabin Constantin Lungu (2B || !2B) <sabinlungu293@gmail.com>
2020-03-04 10:24:00 +0000

    Starting to add Internal Commentary + implementing code for DIY activity 2.

--- app/src/main/java/com/example/weshopapplication/DIYActivityTwo.java
+++ app/src/main/java/com/example/weshopapplication/DIYActivityTwo.java
@@ -1,10 +1,79 @@
 package com.example.weshopapplication;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+// Author of Application: Sabin Constantin Lungu
+// Purpose of Application / Class: Contains the Java code for the DIY activity 2.
+// Date of Last Modification : 04/03/2020
+// Any Errors? None
+
 public class DIYActivityTwo extends AppCompatActivity {
+    private int current_product_id = 1; // The current product id to add to basket (will be incremented)
+    private TextView diyThirdProductTxt; // The first product text
+
+    private ImageView diyThirdProductImg; // Image of the first DIY product
+    private TextView diyThirdProductCost;
+
+    private TextView diyThirdProductColourLbl;
+    private Spinner diyThirdProductColourMenu;
+
+    private TextView diyThirdProductSizeLbl;
+    private Spinner diyThirdProductSizeMenu;
+
+    private TextView diyThirdProductQuantityLbl;
+    private Spinner diyThirdProductQuantityMenu;
+    private Button diyThirdProductToAddToBasketBtn;
+
+    private TextView diyFourthProductTxt;
+    private ImageView diyFourthProductImg;
+
+    private TextView diyFourthProductCost;
+
+    private TextView diyFourthProductColourLbl;
+    private Spinner diyFourthProductColourMenu;
+
+    private TextView diyFourthProductSizeLbl;
+    private Spinner diyFourthProductSizeMenu;
+
+    private TextView diyFourthProductQuantityLbl;
+    private Spinner diyFourthProductQuantityMenu;
+
+    private Button diyFourthProductAddToBasketBtn;
+
+    private double[] diyThirdProductCosts = new double[]{0.00, 50.00, 100.00, 200.00, 400.00, 800.00}; // A double array of product costs for the first DIY product
+    private double[] diySecondProductCosts = new double[]{0.00, 15.00, 30.00, 45.00, 60.00, 75.00};
+
+    private boolean coloursAdded = false;
+    private boolean sizesAdded = false;
+    private boolean quantitiesAdded = false;
+
+    // Adapters for the objects to add to the list
+    private CustomArrayAdapter quantitiesAdapter;
+    private SizeArrayAdapter sizeArrayAdapter;
+    private ColourArrayAdapter coloursAdapter;
+
+    private ArrayList<TechActivity.Colours> diyListOfColoursOne = null; // An array list of colours
+    private ArrayList<Size> diyListOfSizesOne = null;
+    private ArrayList<TechActivity.Quantities> diyListOfQuantitiesOne = null; // An Array list of quantities for the first diy product
+
+    // Creates the array lists for the second DIY product.
+    private ArrayList<TechActivity.Colours> diyListOfColoursTwo = null;
+    private ArrayList<Size> diyListOfSizesTwo = null;
+    private ArrayList<TechActivity.Quantities> diyListOfQuantitiesTwo = null;
+
+    private ImageView cartIcon;
+    private HashMap<Integer, Products> listOfProductsToAddToBasket = new HashMap<Integer, Products>(); // Creates a new hash map of products with an associated ID
+    private Button nextPageBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
