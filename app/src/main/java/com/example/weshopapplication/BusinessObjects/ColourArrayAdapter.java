651e426982f2b286a311e156dca43d36280b54c5
Sabin Constantin Lungu (2B || !2B) <sabinlungu293@gmail.com>
2020-03-08 10:13:17 +0000

    Starting to add internal commentary in the code.

--- app/src/main/java/com/example/weshopapplication/BusinessObjects/ColourArrayAdapter.java
+++ app/src/main/java/com/example/weshopapplication/BusinessObjects/ColourArrayAdapter.java
@@ -12,27 +12,32 @@ import com.example.weshopapplication.ApplicationLayer.TechActivity;
 
 import java.util.ArrayList;
 
-public class ColourArrayAdapter extends ArrayAdapter<TechActivity.Colours> {
-    private Context context;
-    private ArrayList<TechActivity.Colours> listOfColours = null;
+// Author of Application / Class: Sabin Constantin Lungu
+// Purpose of Application / Class: A helper class that allows objects of type colour to be stored in the drop-down menu in the product categories.
+// Date of Last Modification: 08/03/2020
+// Any Errors? None
 
-    public ColourArrayAdapter(Context context, ArrayList<TechActivity.Colours> listOfColours) {
-        super(context, 0, listOfColours);
+public class ColourArrayAdapter extends ArrayAdapter<TechActivity.Colours> { // The colours array adapter class inherits from the array adapter base class
+    private Context context;
+    private ArrayList<TechActivity.Colours> listOfColours = null; // The array list of colours to be stored.
+
+    public ColourArrayAdapter(Context context, ArrayList<TechActivity.Colours> listOfColours) { // Constructor for  the colour array adapter.
+        super(context, 0, listOfColours); // Inherit the context and list of colours
         this.context = context;
         this.listOfColours = listOfColours;
     }
 
     @NonNull
     @Override
-    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) { // Routine that gets the list view at a specific position
         View listOfItems = convertView;
 
-        if (listOfItems == null) {
-            listOfItems = LayoutInflater.from(context).inflate(android.R.layout.simple_spinner_dropdown_item, parent, false);
+        if (listOfItems == null) { // If there is no list of items
+            listOfItems = LayoutInflater.from(context).inflate(android.R.layout.simple_spinner_dropdown_item, parent, false); // Inflate a drop-down menu with the colours
 
             TechActivity.Colours colours = listOfColours.get(position);
         }
 
-        return listOfItems;
+        return listOfItems; // Returns the list of items.
     }
 }
