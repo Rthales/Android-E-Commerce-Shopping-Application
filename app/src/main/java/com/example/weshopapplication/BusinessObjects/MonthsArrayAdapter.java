51463b040ed0886039eb7a399f93596fc6bbfa78
Sabin Constantin Lungu (2B || !2B) <sabinlungu293@gmail.com>
2020-06-09 16:47:16 +0100

    Update MonthsArrayAdapter.java

--- app/src/main/java/com/example/weshopapplication/BusinessObjects/MonthsArrayAdapter.java
+++ app/src/main/java/com/example/weshopapplication/BusinessObjects/MonthsArrayAdapter.java
@@ -5,9 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-
 import androidx.annotation.NonNull;
-
 import java.util.ArrayList;
 
 // Author of Application / Class: Sabin Constantin Lungu
