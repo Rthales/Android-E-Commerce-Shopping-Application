137c03b30449058957f759f22887e48129030cbd
Sabin Constantin Lungu (2B || !2B) <sabinlungu293@gmail.com>
2020-06-09 16:47:49 +0100

    Update QuantitiesArrayAdapter.java

--- app/src/main/java/com/example/weshopapplication/BusinessObjects/QuantitiesArrayAdapter.java
+++ app/src/main/java/com/example/weshopapplication/BusinessObjects/QuantitiesArrayAdapter.java
@@ -5,11 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-
 import androidx.annotation.NonNull;
-
 import com.example.weshopapplication.ApplicationLayer.TechActivity;
-
 import java.util.ArrayList;
 
 // Author of Application / Class: Sabin Constantin Lungu
